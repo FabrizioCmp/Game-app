859e2ae6adfd48651ada4e5aa398126658b4b36c
FabrizioCmp <fabriziocampari01@gmail.com>
2023-02-26 11:07:33 +0100

    homePage styling & score styling

--- resources/views/gameHome.blade.php
+++ resources/views/gameHome.blade.php
@@ -1,27 +1,17 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Snake</title>
-    @vite(['resources/scss/app.scss','resources/js/main.js','resources/js/app.js'])
-</head> --}}
-@extends("layouts.app")
-@section("content")
-    
-<body>
-    <h1 class="title_game">Snake</h1>
-    <canvas id="board">
-        
-    </canvas>
-    <div class="score_container">
-        <h1>SCORE <span id="score" ></span></h1>
+@extends('layouts.app')
+@section('content')
 
-    </div>
-    
-</body>
+    <body>
+        <h1 class="title_game">Snake</h1>
 
+        <canvas id="board">
+        </canvas>
+
+        <div class="score_container">
+            <span class="block_score"></span>
+             <span id="score"></span>
+        </div>
+
+    </body>
 @endsection
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="walcomePage">
-        <div class="container my-4 ">
+        <div class="container py-4 ">
             <h1 style="font-size: 150px" class="lh-1">Hi!</h1>
             <h2 class="">Welcome to your favorite game!</h2>
             <h4 class="">If you alrady have an account Login end play, if you don't plaese register.</h4>
